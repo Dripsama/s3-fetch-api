07cd79c344c9195182e474af7d6fad92d885518f
Nikhil <nikhil@rhombuz.io>
2023-10-16 18:17:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const aws = require("aws-sdk");
+const csv = require("csv-parser");
+const app = express();
+const port = process.env.PORT || 8888;
+
+require("dotenv").config();
+
+const s3BucketName = process.env.BUCKET_NAME;
+
+// Create an S3 object
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  Bucket: s3BucketName,
+});
+
+app.get("/api/csv", (req, res) => {
+  const s3FileName = req.query.file + ".csv";
+
+  const s3KeyPrefix = "csv-data/";
+
+  const params = {
+    Bucket: s3BucketName,
+    Key: s3KeyPrefix + s3FileName,
+  };
+
+  res.setHeader("Content-Type", "text/csv");
+  res.setHeader("Content-Disposition", `attachment; filename=${s3FileName}`);
+
+  s3.getObject(params).createReadStream().pipe(res);
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
